f653e7c06b13fb930cf1c23c2d37e6e8051702ee
Berk Kütükçüoğlu <berkkutukcuoglu@gmail.com>
2023-05-15 17:53:07 +0300

    first commit

---
+++ courses/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index),
+    path('<kurs_adi>', views.details),
+    path('kategori/<int:category_id>', views.getCoursesById),
+    path('kategori/<str:category_name>', views.getCoursesByCategory, name='courses_by_category'),
+]
---
+++ courses/views.py
@@ -0,0 +1,87 @@
+from datetime import date,datetime
+from django.shortcuts import redirect, render
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
+
+# Create your views here.
+
+data = {
+    "programlama": "Programlama kategorisindeki kurslar",
+     "web-gelistirme": "Web Geliştirme kategorisindeki kurslar",
+     "mobil": "mobil kategorisindeki kurslar",
+}
+
+db = {
+    "courses": [
+        {
+            "title": "javascript kursu",
+            "description": "javascript kurs açıklaması",
+            "imageUrl": "1.jpg",
+            "slug": "javascipt-kursu",
+            "date": datetime.now(),
+            "isActive": True,
+            "isUpdated": False
+        },
+        {
+            "title": "python kursu",
+            "description": "python kurs açıklaması",
+            "imageUrl": "2.jpg",
+            "slug": "python-kursu",
+            "date": date(2022,9,10),
+            "isActive": False,
+            "isUpdated": False
+        },
+        {
+            "title": "web geliştirme kursu",
+            "description": "web geliştirme kurs açıklaması",
+            "imageUrl": "3.jpg",
+            "slug": "web-gelistirme-kursu",
+            "date": date(2022,8,10),
+            "isActive": True,
+            "isUpdated": True
+        }
+    ],
+    "categories": [
+        {"id": 1, "name": "programlama", "slug": "programlama"},
+        {"id": 2, "name": "web geliştirme", "slug": "web-geliştirme"},
+        {"id": 3, "name": "mobil uygulamalar", "slug": "mobil-uygulamalar"}
+        ]
+}
+
+def index(request):
+    #list comphension
+    kurslar = [course for course in db["courses"] if course["isActive"]]
+    kategoriler = db["categories"]
+
+    # for kurs in db["courses"]:
+    #     if kurs["isActive"]:
+    #         kurslar.append(kurs)
+
+    return render(request, 'courses/index.html', {
+        'categories': kategoriler,
+        'courses': kurslar
+    })
+
+def details(request, kurs_adi):
+    return HttpResponse(f'{kurs_adi} detay sayfası ')
+
+def getCoursesByCategory(request, category_name):
+    try:
+        category_text = data[category_name]
+        return render(request, 'courses/kurslar.html', {
+            'category': category_name,
+            'category_text': category_text
+        })
+    except:
+        return HttpResponseNotFound("<h1>yanlış kategori seçimi</h1>")  
+
+def getCoursesById(request, category_id):
+    category_list = list(data.keys())
+    if(category_id > len(category_list)):
+        return HttpResponseNotFound("yanlış kategori seçimi")
+    category_name = category_list[category_id - 1]
+
+    redirect_url = reverse('courses_by_category', args=[category_name])
+
+    return redirect(redirect_url)
+
